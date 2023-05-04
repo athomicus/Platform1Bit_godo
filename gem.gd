0bb5ac394f902593e9720e5ed9e02817c2174507
athomicus <athomicus@gmail.com>
2023-05-04 12:38:20 +0200

    gems added

---
+++ gem.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		Global.gems_collected +=1
+		#print("Gem",Global.gems_collected )
+		queue_free()
--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -31,7 +31,7 @@ func _physics_process(delta):
 			$AnimatedSprite2D.flip_h = false 
 			
 	else:
-		print("idle:",direction)
+		#print("idle:",direction)
 		if not is_on_floor(): 
 			$AnimatedSprite2D.play("jump")
 		else:
